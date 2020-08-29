2ab778001c745053e823f5d66f309c14b666701d
sumana2001 <63084088+sumana2001@users.noreply.github.com>
2020-08-30 00:23:00 +0530

    folder created

---
+++ Horizontal bar/new.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style123.css">
+    <title>Document</title>
+</head>
+<body>
+    
+        <div class="percent">
+            <div class="progress">
+                <h3 style="margin-left:10px; margin-top: 7px; color:white;">Education</h3>
+            </div>
+        </div>
+    
+</body>
+</html>
