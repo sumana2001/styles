16d24eb87b98b6fea0a228e82abc1212f66ddee3
Aniket <aniketindian8@gmail.com>
2020-09-03 22:45:56 +0530

    Initial

---
+++ Neon Social icon/main.py
@@ -0,0 +1,26 @@
+
+x= int(input())
+y=int(input())
+arr=[]
+mx=0
+my=0
+def print_factors(x):
+   for i in range(1, x + 1):
+       if x % i == 0:
+           m=i
+           if m>mx:
+               mx=m
+    return mx
+
+           
+def print_factors(y):
+   for i in range(1, y+ 1):
+       if x % i == 0:
+           m=i
+           if m>my:
+               my=m
+
+
+
+
+
