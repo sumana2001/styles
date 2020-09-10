3fcd0cb8ac6034d4f302c44009f9624bed919cb9
Aniket <aniketindian8@gmail.com>
2020-09-10 14:54:55 +0530

    initial

---
+++ Counter/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.counter').each(function () {
+    $(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+    }, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+    $(this).text(Math.ceil(now));
+    }
+    });
+    });
+    
+    });
